bf3a8ac2e9186552867557a309b553d85d52eedb
ChellyJam <dx15622133362@gmail.com>
2017-10-19 21:34:54 +0800

    add matrix transformation functions to JexMath

--- SPH/JexMath.h
+++ SPH/JexMath.h
@@ -9,14 +9,15 @@
 
 namespace Jex {
 
+#pragma region Define
+
 #define EPSILON 0.000001
-#define PI 3.1415926535
+#define PI 3.1415926535898
 
 #define Max(a, b) (((a) > (b)) ? (a) : (b))
 #define Min(a, b) (((a) < (b)) ? (a) : (b))
 
-	class Float3;
-	class Vector3f;
+#pragma endregion
 
 #pragma region Float3
 
@@ -192,8 +193,24 @@ namespace Jex {
 		Matrix4x4f & operator -= (const Matrix4x4f & mtrx);
 		Matrix4x4f & operator *= (const Matrix4x4f & mtrx);
 
+		// 计算行列式的值
+		float Det() const;
+
+		// 判断矩阵是否可逆
+		bool IsInvertible() const;
+
 		void print_info() const;
 	};
+
+	// 生成单位矩阵
+	inline Matrix4x4f ProductIdentityMtrx() {
+		Matrix4x4f i_mtrx;
+		i_mtrx.SetData(0, 1.0);
+		i_mtrx.SetData(5, 1.0);
+		i_mtrx.SetData(10, 1.0);
+		i_mtrx.SetData(15, 1.0);
+		return i_mtrx;
+	}
 
 #pragma endregion
 
@@ -293,6 +310,146 @@ namespace Jex {
 		return v4;
 	}
 
+
+#pragma endregion
+
+#pragma region MatrixTransformation
+
+	float RadianToDegree(float radian);
+	float DegreeToRadian(float degree);
+
+	Matrix4x4f Translate(float dx, float dy, float dz);
+	Matrix4x4f TranslateX(float dx);
+	Matrix4x4f TranslateY(float dy);
+	Matrix4x4f TranslateZ(float dz);
+
+	// 注意缩放函数不能使模型的位置发生偏移 
+	// 即要将模型移动到原点，然后进行缩放，再移动到原来的位置
+	Matrix4x4f Scale(float dx, float dy, float dz, Float4 p, Float4 o = Float4(0.0, 0.0, 0.0, 0.0));
+	Matrix4x4f Scale(float ds, Float4 p, Float4 o = Float4(0.0, 0.0, 0.0, 0.0));
+	Matrix4x4f ScaleX(float dx, Float4 p, Float4 o = Float4(0.0, 0.0, 0.0, 0.0));
+	Matrix4x4f ScaleY(float dy, Float4 p, Float4 o = Float4(0.0, 0.0, 0.0, 0.0));
+	Matrix4x4f ScaleZ(float dz, Float4 p, Float4 o = Float4(0.0, 0.0, 0.0, 0.0));
+
+	// 注意旋转函数不能使模型的位置发生偏移
+	// 即要将模型移动到原点，然后进行旋转，再移动到原来的位置
+	Matrix4x4f Rotate(float dx, float dy, float dz, Float4 p, Float4 o = Float4(0.0, 0.0, 0.0, 0.0));
+	Matrix4x4f RotateX(float dx, Float4 p, Float4 o = Float4(0.0, 0.0, 0.0, 0.0));
+	Matrix4x4f RotateY(float dy, Float4 p, Float4 o = Float4(0.0, 0.0, 0.0, 0.0));
+	Matrix4x4f RotateZ(float dz, Float4 p, Float4 o = Float4(0.0, 0.0, 0.0, 0.0));
+
+	float RadianToDegree(float radian) {
+		return 180.0 * radian / PI;
+	}
+
+	float DegreeToRadian(float degree) {
+		return PI * degree / 180.0;
+	}
+
+	Matrix4x4f Translate(float dx, float dy, float dz) {
+		Matrix4x4f i_mtrx = ProductIdentityMtrx();
+		i_mtrx.SetData(3, dx);
+		i_mtrx.SetData(7, dy);
+		i_mtrx.SetData(11, dz);
+		return i_mtrx;
+	}
+
+	Matrix4x4f TranslateX(float dx) {
+		return Translate(dx, 0.0, 0.0);
+	}
+	
+	Matrix4x4f TranslateY(float dy) {
+		return Translate(0.0, dy, 0.0);
+	}
+
+	Matrix4x4f TranslateZ(float dz) {
+		return Translate(0.0, 0.0, dz);
+	}
+
+	Matrix4x4f Scale(float dx, float dy, float dz, Float4 p, Float4 o) {
+
+		// p 到 o 的平移矩阵
+		Matrix4x4f po_mtrx = Translate(o.x - p.x, o.y - p.y, o.z - p.z);
+
+		// 缩放矩阵
+		Matrix4x4f scale_mtrx = ProductIdentityMtrx();
+		scale_mtrx.SetData(0, dx);
+		scale_mtrx.SetData(5, dy);
+		scale_mtrx.SetData(10, dz);
+		
+		// o 到 p 的平移矩阵
+		Matrix4x4f op_mtrx = Translate(p.x - o.x, p.y - o.y, p.z - o.z);
+
+		return op_mtrx * scale_mtrx * po_mtrx;
+	}
+
+	Matrix4x4f Scale(float ds, Float4 p, Float4 o) {
+		return Scale(ds, ds, ds, p, o);
+	}
+
+	Matrix4x4f ScaleX(float dx, Float4 p, Float4 o) {
+		return Scale(dx, 0.0, 0.0, p, o);
+	}
+
+	Matrix4x4f ScaleY(float dy, Float4 p, Float4 o) {
+		return Scale(0.0, dy, 0.0, p, o);
+	}
+
+	Matrix4x4f ScaleZ(float dz, Float4 p, Float4 o) {
+		return Scale(0.0, 0.0, dz, p, o);
+	}
+
+	Matrix4x4f Rotate(float dx, float dy, float dz, Float4 p, Float4 o) {
+
+		// p 到 o 的平移矩阵
+		Matrix4x4f po_mtrx = Translate(o.x - p.x, o.y - p.y, o.z - p.z);
+
+		// 沿自身 x 轴旋转
+		Matrix4x4f rx_mtrx = ProductIdentityMtrx();
+		if (abs(dx) < EPSILON) {
+			rx_mtrx.SetData(5, cos(DegreeToRadian(dx)));
+			rx_mtrx.SetData(6, -sin(DegreeToRadian(dx)));
+			rx_mtrx.SetData(9, sin(DegreeToRadian(dx)));
+			rx_mtrx.SetData(10, cos(DegreeToRadian(dx)));
+		}
+
+		// 沿自身 y 轴旋转
+		Matrix4x4f ry_mtrx = ProductIdentityMtrx();
+		if (abs(dy) < EPSILON) {
+			ry_mtrx.SetData(0, cos(DegreeToRadian(dy)));
+			ry_mtrx.SetData(2, -sin(DegreeToRadian(dy)));
+			ry_mtrx.SetData(8, sin(DegreeToRadian(dy)));
+			ry_mtrx.SetData(10, cos(DegreeToRadian(dy)));
+		}
+
+		Matrix4x4f rz_mtrx = ProductIdentityMtrx();
+		if (abs(dz) < EPSILON) {
+			rz_mtrx.SetData(0, cos(DegreeToRadian(dz)));
+			rz_mtrx.SetData(1, -sin(DegreeToRadian(dz)));
+			rz_mtrx.SetData(4, sin(DegreeToRadian(dz)));
+			rz_mtrx.SetData(5, cos(DegreeToRadian(dz)));
+		}
+
+		// o 到 p 的平移矩阵
+		Matrix4x4f op_mtrx = Translate(p.x - o.x, p.y - o.y, p.z - o.z);
+
+		return op_mtrx * rz_mtrx * ry_mtrx * rx_mtrx * po_mtrx;
+	}
+
+
+	Matrix4x4f RotateX(float dx, Float4 p, Float4 o) {
+		return Rotate(dx, 0.0, 0.0, p, o);
+	}
+
+
+	Matrix4x4f RotateY(float dy, Float4 p, Float4 o) {
+		return Rotate(0.0, dy, 0.0, p, o);
+	}
+
+
+	Matrix4x4f RotateZ(float dz, Float4 p, Float4 o) {
+		return Rotate(0.0, 0.0, dz, p, o);
+	}
 
 #pragma endregion
 
