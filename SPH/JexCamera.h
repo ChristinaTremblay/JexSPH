0c6a30809b680bc535f146d1824e8689cd554ac3
ChellyJam <dx15622133362@gmail.com>
2017-10-19 19:28:55 +0800

    add JexCamera.h and JexCamera.cpp

---
+++ SPH/JexCamera.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#ifndef JEX_CAMERA_H
+#define JEX_CAMERA_H
+
+#include "JexMath.h"
+
+namespace Jex {
+
+	class JexCamera {
+	public:
+		JexCamera();
+		~JexCamera();
+		                                         
+
+	};
+
+}
+
+#endif
